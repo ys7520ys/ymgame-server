87d86cb3e9583b02717dcebf6fcb3214613bf5b9
ys7520ys <ys7520ys@naver.com>
2026-08-13 19:36:22 +0900

    Add player collision

--- public/main.js
+++ public/main.js
@@ -4112,44 +4112,6 @@ function resolveWorldCollision(
 // 벽 / 스피커 충돌 검사
 // ==================================================
 
-function resolveWorldCollision(
-    player,
-    previousPosition
-) {
-
-    // 플레이어 몸통 반경
-    const playerRadius = 0.38;
-
-    const x =
-        player.root.position.x;
-
-    const z =
-        player.root.position.z;
-
-
-    for (const wall of wallColliders) {
-
-        const collision =
-            x + playerRadius > wall.minX &&
-            x - playerRadius < wall.maxX &&
-            z + playerRadius > wall.minZ &&
-            z - playerRadius < wall.maxZ;
-
-
-        if (collision) {
-
-            // 일단 이전 위치로 되돌림
-            player.root.position.x =
-                previousPosition.x;
-
-            player.root.position.z =
-                previousPosition.z;
-
-            return;
-        }
-    }
-}
-
 
 function movePlayer() {
 
