a324210b7164e0faa6b768024197e04fd7f70d4f
ys7520ys <ys7520ys@naver.com>
2026-08-15 21:06:59 +0900

    Add player collision

--- public/main.js
+++ public/main.js
@@ -1705,13 +1705,13 @@ function createWallArtwork(
 
 const leftWallArtworks = [
     "./assets/코딱지.png",
-    "./assets/여르미바보.png",
+    "./assets/엶아바라.png",
 ];
 
 // ★ 각 작품 옆 네임택
 const leftWallNameTags = [
     "./assets/코딱지네임.png",
-    "./assets/네임.png",
+    "./assets/엶아바라네임.png",
 ];
 
 // ==================================================
